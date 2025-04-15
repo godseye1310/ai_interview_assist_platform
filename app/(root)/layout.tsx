4aca87ba7f5ed4616acadaa9e6f015f8d4084dc2
Avinash <avinashkmg1310@gmail.com>
2025-04-15 06:33:01 +0530

    -

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -19,7 +19,7 @@ const Rootlayout = async ({ children }: { children: React.ReactNode }) => {
 					className="flex items-center gap-2 text-primary-100"
 				>
 					<Image src="/logo.svg" alt="logo" width={38} height={32} />
-					<h2 className="">Prepwise</h2>
+					<h2 className="">VoicePrep</h2>
 				</Link>
 			</nav>
 			{children}
